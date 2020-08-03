6baaf8e039ade8cdae1ae9a9d1aef9f00e3c1ed2
Liberato Aguilar <liberatoalanaguilar@gmail.com>
2020-08-02 21:46:21 -0600

    Added save button and clear save to save history to local storage.

--- tracker.js
+++ tracker.js
@@ -3,6 +3,8 @@ let switcherTable = document.getElementById("switcherTable");
 let currentMonth = document.getElementById("currentMonth");
 let backButton = document.getElementById("back");
 let forButton = document.getElementById("forward");
+let saveButton = document.getElementById("save");
+let clearSaveButton = document.getElementById("clearsave");
 
 let today = new Date();
 let month = today.toLocaleString('default', { month: 'long' });
@@ -11,15 +13,29 @@ let todayString = month.toString()+"-"+today.getFullYear().toString();
 let currentDate = today;
 
 //formatEx  {July_2020:{2:["yellow","nothing"]}};
-let savedItems = localStorage.getItem("savedItems") || {};
-let locallySaved = {July_2020:{2:["yellow","nothing"]}};
+let savedItems = JSON.parse(localStorage.getItem("savedItems")) || {};
+
+function save() {
+  saveLocal(currentDate.toLocaleString('default', { month: 'long' }),currentDate.getFullYear())
+  localStorage.setItem("savedItems",JSON.stringify(savedItems));
+  saveButton.textContent = "Saved!";
+  setTimeout(() => {saveButton.textContent = "Save";}, 2000);
+}
+saveButton.addEventListener("click",save);
+
+function clearSave() {
+  localStorage.clear();
+  clearSaveButton.textContent = "Cleared!";
+  setTimeout(() => {clearSaveButton.textContent = "Clear Save";},2000);
+}
+clearSaveButton.addEventListener("click",clearSave);
 
 function back(){
   saveLocal(currentDate.toLocaleString('default',{ month: 'long'}),currentDate.getFullYear());
   deleteCells();
   currentDate.setMonth(currentDate.getMonth()-1);
   createDayCells(currentDate);
-  load(currentDate.toLocaleString('default', {month: 'long'}),currentDate.getFullYear(),locallySaved);
+  load(currentDate.toLocaleString('default', {month: 'long'}),currentDate.getFullYear(),savedItems);
   currentMonth.textContent = currentDate.toLocaleString('default', {month: 'long'}).toString()+" "+currentDate.getFullYear().toString();
 }
 backButton.addEventListener("click",back);
@@ -29,7 +45,7 @@ function forward(){
   deleteCells();
   currentDate.setMonth(currentDate.getMonth()+1);
   createDayCells(currentDate);
-  load(currentDate.toLocaleString('default', {month: 'long'}),currentDate.getFullYear(),locallySaved);
+  load(currentDate.toLocaleString('default', {month: 'long'}),currentDate.getFullYear(),savedItems);
   currentMonth.textContent = currentDate.toLocaleString('default', {month: 'long'}).toString()+" "+currentDate.getFullYear().toString();
 }
 forButton.addEventListener("click",forward);
@@ -55,7 +71,7 @@ function saveLocal(month,year) {
   for (let day of red) {
     savobj[day.textContent] = ["red"];
   }
-  locallySaved[month+"_"+year.toString()] = savobj;
+  savedItems[month+"_"+year.toString()] = savobj;
 
 }
 
@@ -115,7 +131,7 @@ function createDayCells(givenDate) {
   }
   for (let day of Array.from(document.getElementsByClassName("day"))){
     day.setAttribute("id","none");
-    day.addEventListener("click",() => changeColor(day));
+    if (day.textContent != "") day.addEventListener("click",() => changeColor(day));
   }
 
 }
