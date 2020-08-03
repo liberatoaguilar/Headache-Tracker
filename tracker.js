01869decb6336bc1bb2c63c11e07eee303766f0c
Liberato Aguilar <liberatoalanaguilar@gmail.com>
2020-08-02 21:19:10 -0600

    New features: click on days to cycle colors and arrows to cycle months.

--- tracker.js
+++ tracker.js
@@ -1,14 +1,71 @@
 let calendarTable = document.getElementById("calendarTable");
 let switcherTable = document.getElementById("switcherTable");
 let currentMonth = document.getElementById("currentMonth");
+let backButton = document.getElementById("back");
+let forButton = document.getElementById("forward");
 
 let today = new Date();
 let month = today.toLocaleString('default', { month: 'long' });
 currentMonth.textContent = month.toString()+" "+today.getFullYear().toString();
 let todayString = month.toString()+"-"+today.getFullYear().toString();
+let currentDate = today;
 
-//formatEx  {Aug-2020:{2:["mild","nothing"]},{25:["bad","ibuprofen"]}};
-let savedItems = localStorage.getItem("savedItems") || {todayString};
+//formatEx  {July_2020:{2:["yellow","nothing"]}};
+let savedItems = localStorage.getItem("savedItems") || {};
+let locallySaved = {July_2020:{2:["yellow","nothing"]}};
+
+function back(){
+  saveLocal(currentDate.toLocaleString('default',{ month: 'long'}),currentDate.getFullYear());
+  deleteCells();
+  currentDate.setMonth(currentDate.getMonth()-1);
+  createDayCells(currentDate);
+  load(currentDate.toLocaleString('default', {month: 'long'}),currentDate.getFullYear(),locallySaved);
+  currentMonth.textContent = currentDate.toLocaleString('default', {month: 'long'}).toString()+" "+currentDate.getFullYear().toString();
+}
+backButton.addEventListener("click",back);
+
+function forward(){
+  saveLocal(currentDate.toLocaleString('default',{ month: 'long'}),currentDate.getFullYear());
+  deleteCells();
+  currentDate.setMonth(currentDate.getMonth()+1);
+  createDayCells(currentDate);
+  load(currentDate.toLocaleString('default', {month: 'long'}),currentDate.getFullYear(),locallySaved);
+  currentMonth.textContent = currentDate.toLocaleString('default', {month: 'long'}).toString()+" "+currentDate.getFullYear().toString();
+}
+forButton.addEventListener("click",forward);
+
+function load(month,year,search){
+  for (let day of Array.from(document.getElementsByClassName("day"))) {
+    if (search[month+"_"+year.toString()] == undefined) console.log('skip');
+    else if (search[month+"_"+year.toString()][day.textContent] != undefined) day.setAttribute("id",search[month+"_"+year][day.textContent][0]);
+  }
+}
+
+function saveLocal(month,year) {
+  let savobj = {};
+  let yellow = document.querySelectorAll("#yellow");
+  for (let day of yellow) {
+    savobj[day.textContent] = ["yellow"];
+  }
+  let orange = document.querySelectorAll("#orange");
+  for (let day of orange) {
+    savobj[day.textContent] = ["orange"];
+  }
+  let red = document.querySelectorAll("#red");
+  for (let day of red) {
+    savobj[day.textContent] = ["red"];
+  }
+  locallySaved[month+"_"+year.toString()] = savobj;
+
+}
+
+function changeColor(day) {
+  if (day.getAttribute("id") == "none") day.setAttribute("id","yellow");
+  else if (day.getAttribute("id") == "yellow") day.setAttribute("id","orange");
+  else if (day.getAttribute("id") == "orange") day.setAttribute("id","red");
+  else if (day.getAttribute("id") == "red") day.setAttribute("id","none");
+  saveLocal(1,1);
+}
 
 function createDayCells(givenDate) {
   let calHeight = calendarTable.clientHeight;
@@ -40,6 +97,7 @@ function createDayCells(givenDate) {
       calendarTable.appendChild(newWeek);
     }
     let newDay = document.createElement("td");
+    newDay.setAttribute("class","day");
     newDay.textContent = todayDate.getDate();
     todayDate.setDate(todayDate.getDate()+1);
     newWeek.appendChild(newDay);
@@ -53,7 +111,19 @@ function createDayCells(givenDate) {
   for (let week of weeks) {
     week.style.height = (bodyHeight-(calHeight+switcherHeight))/weeks.length-2;
   }
+  for (let day of Array.from(document.getElementsByClassName("day"))){
+    day.setAttribute("id","none");
+    day.addEventListener("click",() => changeColor(day));
+  }
 
 }
 
+function deleteCells() {
+  let weeks = document.getElementsByClassName("week");
+  for (let weekChild of Array.from(weeks)) {
+    calendarTable.removeChild(weekChild);
+  }
+}
+
 createDayCells(today);
+load(month,today.getFullYear(),savedItems);
